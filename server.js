851d1081dfaa720f7ad0cc18df406c401027eeaa
Vitaly Homonov <avg.idm@gmail.com>
2016-12-10 23:30:30 +0200

    add simple static server

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const static = require('node-static');
+
+const file = new static.Server('.');
+
+http.createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response);
+  }).resume();
+}).listen(3456);
