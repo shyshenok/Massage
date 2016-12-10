6704878c17d5c76b5706bd90125d278a910c24de
Vitaly Homonov <avg.idm@gmail.com>
2016-12-10 23:39:50 +0200

    add run commands

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-
 const static = require('node-static');
-
 const file = new static.Server('.');
 
 http.createServer(function (request, response) {
   request.addListener('end', function () {
     file.serve(request, response);
   }).resume();
-}).listen(3456);
+}).listen(process.env.PORT || 3000);
